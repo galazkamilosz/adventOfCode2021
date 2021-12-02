84921caafa7d69f739ac2a364058ed197ce0a68a
Milosz Galazka <m.galazkamilosz@gmail.com>
2021-12-02 01:18:25 +0100

    1st day 2 part

--- 1/main.go
+++ 1/main.go
@@ -9,6 +9,56 @@ import (
 )
 
 func main() {
+	fmt.Println(secondPart())
+}
+
+func secondPart() int {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	differencesCounter := 0
+
+	recorder := make([]int, 0, 4)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		currentElement, err := strconv.Atoi(text)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if len(recorder) > 3 {
+			differencesCounter += compareSums(recorder[:3], recorder[1:])
+			recorder = recorder[1:]
+		}
+		recorder = append(recorder, currentElement)
+	}
+	return differencesCounter
+}
+
+func compareSums(base, next []int) int {
+	sumBase := 0
+	sumNext := 0
+	for _, v := range base {
+		sumBase += v
+	}
+
+	for _, v := range next {
+		sumNext += v
+	}
+	if sumNext > sumBase {
+		return 1
+	}
+	return 0
+}
+
+func firstPart() int {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -36,5 +86,5 @@ func main() {
 		}
 		mem = current
 	}
-	fmt.Println(counter)
+	return counter
 }
